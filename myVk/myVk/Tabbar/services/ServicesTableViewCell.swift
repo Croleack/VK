d5751bcf85eceb16dfe58dc3d714d2935da94dae
Радик Хазиев <121854191+Croleack@users.noreply.github.com>
2023-08-06 22:31:45 +0500

    step 3

---
+++ myVk/myVk/Tabbar/services/ServicesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ServicesTableViewCell.swift
+//  myVk
+//
+//  Created by Enzhe Gaysina on 06.08.2023.
+//
+
+import UIKit
+
+class ServicesTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var pic: UIImageView!
+    @IBOutlet weak var menuName: UILabel!
+    
+    
+    
+    
+    
+    
+    
+    
+}
--- myVk/myVk/Tabbar/services/ServicesViewController.swift
+++ myVk/myVk/Tabbar/services/ServicesViewController.swift
@@ -7,23 +7,28 @@
 
 import UIKit
 
-class ServicesViewController: UIViewController {
+//необходимо подписаться еще на два протокола, чтобы работать с таблицами
+class ServicesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
 
+    let cells = ["Друзья", "Сообщества", "Музыка"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //этот метод показывает количество ячеек в нашей таблице
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //вернем количество наших ячеек
+        return cells.count
+        
     }
-    */
+    //метод показывает какя где строка будет расположена
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ServicesTableViewCell
+        cell.menuName.text = cells[indexPath.row]
+        return cell
+    }
 
 }
