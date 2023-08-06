5e333d81326e504e1d7b40b11d0e81e4f7010461
Радик Хазиев <121854191+Croleack@users.noreply.github.com>
2023-08-06 15:39:21 +0500

    step 1

---
+++ myVk/myVk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myVk
+//
+//  Created by Enzhe Gaysina on 06.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
